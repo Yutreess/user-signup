de96162b1f88058b6de3eed59a2a957c4feef752
Justus <justus_pete@hotmail.com>
2019-04-04 20:20:32 -0500

    Added check for username's existence.

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ def index():
 def welcome():
   username = request.form['username']
   mismatch = False
+  no_uname = False
+
+  # Check if Username is empty
+  if not username:
+    no_uname = True
+    return render_template("signup.html", no_uname=no_uname)
+
+  # Check if passwords match
   if request.form['pass1'] == request.form['pass2']:
     return render_template("welcome.html", username=username)
   else:
     mismatch = True
-    return render_template('signup.html', mismatch=mismatch)
+    return render_template("signup.html", mismatch=mismatch)
 
 app.run()
