a6251afb859edfcc8de1fdd1b99505623b914fa7
Justus <justus_pete@hotmail.com>
2019-04-04 20:08:56 -0500

    Added checks to passwords matching.

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/signup", methods=['GET', 'POST'])
 def index():
   return render_template('signup.html')
+
+
+@app.route("/welcome", methods=['POST'])
+def welcome():
+  username = request.form['username']
+  mismatch = False
+  if request.form['pass1'] == request.form['pass2']:
+    return render_template("welcome.html", username=username)
+  else:
+    mismatch = True
+    return render_template('signup.html', mismatch=mismatch)
 
 app.run()
